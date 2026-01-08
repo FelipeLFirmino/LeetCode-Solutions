4a875d9a765ea0389a6449f4f8168e0f7133974a
Felipe Firmino <lipe19x@gmail.com>
2026-01-08 20:46:11 -0300

    minor changes in comments

--- 88_merge_sorted_array.go
+++ 88_merge_sorted_array.go
@@ -1,19 +1,18 @@
-//Para resolver esse problema iteramos sobre os arrays de tras para frente, com dois indices(um para cada array)
-// e um terceiro index, para sabermos o ultimo valor valido no array 1 (que é sempre maior)
-// toda iteração deve adicionar um valor ao indice atual de nums1( o index que começou no final) e fazer pelo
-//menos uma alteração  no array de nums1, SEMPRE COM O MAIOR VALOR PRESENTE NO LOOP (vindo ou de nums1 ou nums2)
+package leetcodesolutions
 
+// To solve this problem, we iterate through the arrays from back to front using two indices (one for each array)
+// // and a third index to track the current position in the total length of nums1.
+// // Every iteration must add a value to the current index of nums1 (starting from the end)
+// // always choosing the GREATEST VALUE AVAILABLE in the loop (either from nums1 or nums2).
 func merge(nums1 []int, m int, nums2 []int, n int) {
-	p1 := m - 1 // Último elemento válido em nums1
-	p2 := n - 1 // Último elemento em nums2
-	p := m + n - 1 // Última posição total de nums1
+	p1 := m - 1
+	p2 := n - 1
+	p := m + n - 1
 	for p2 >= 0 {
-		// Se ainda houver elementos em nums1 e o valor em nums1 for maior
 		if p1 >= 0 && nums1[p1] > nums2[p2] {
 			nums1[p] = nums1[p1]
 			p1--
 		} else {
-			// Caso contrário, ou se nums1 acabou, pega de nums2
 			nums1[p] = nums2[p2]
 			p2--
 		}
